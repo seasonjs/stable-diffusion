46d5ce428ab39b8495183cfa944fdf77354252a1
Cyberhan123 <255542417@qq.com>
2023-09-20 23:28:12 +0800

    add c style dylib binding

---
+++ embed_windows.go
@@ -0,0 +1,8 @@
+// Copyright (c) seasonjs. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+//go:build windows && amd64
+
+package stable_diffusion
+
+//TODO support windows
