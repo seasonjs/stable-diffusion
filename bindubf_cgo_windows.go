f18614f276dcc74f02bb86263cbaefe3f3c66e35
Cyberhan123 <255542417@qq.com>
2024-01-17 18:08:14 +0800

    rm cgo windows

--- bindubf_cgo_windows.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) seasonjs. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-
-//go:build cgo && windows
-
-package sd
